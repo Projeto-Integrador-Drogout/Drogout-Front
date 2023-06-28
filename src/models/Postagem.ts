7b0b5f659c899dceec7d3151679cac9f07a1d8db
Samuel123n <souzasamuka2017@gmail.com>
2023-06-28 09:09:30 -0300

    implementação crud

---
+++ src/models/Postagem.ts
@@ -0,0 +1,8 @@
+import Tema from './Tema'
+
+interface Postagem{
+    id: number;
+    titulo: string;
+    texto: string;
+    tema?: Tema | null
+} export default Postagem;
---
+++ src/store/Store.ts
@@ -0,0 +1,7 @@
+import {createStore} from 'redux';
+import { tokenReducer } from './tokens/TokensReducer';
+
+
+const store = createStore(tokenReducer);
+
+export default store;
