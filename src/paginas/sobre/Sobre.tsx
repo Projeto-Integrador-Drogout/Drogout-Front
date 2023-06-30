094c44af3e3ce19052f5a090744e875504734fe8
Lucas Pinheiro <lucaspinheiros2@hotmail.com>
2023-06-30 15:30:25 -0300

    estilização da pagina Sobre

--- src/paginas/sobre/Sobre.tsx
+++ src/paginas/sobre/Sobre.tsx
@@ -1,33 +1,24 @@
 import React from "react";
-import { Typography, Grid, Button, Box } from "@material-ui/core";
+import { Typography, Grid, Box } from "@material-ui/core";
 import "./Sobre.css";
 
 function Sobre() {
-
-  
   return (
     <>
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        className="caixa"
-      >
-        <Grid alignItems="center" item xs={6}>
-          <Box paddingX={10}>
-            <Typography
-              variant="h3"
-              gutterBottom
-              color="textPrimary"
-              component="h3"
-              align="center"
-              className="titulo"
-            >
-              Sobre nós!
-            </Typography>
-            
-        
+      <div className="caixa">
+        <Grid container justifyContent="center" alignItems="center">
+          <Grid item xs={12} md={6}>
+            <Box paddingX={10}>
+              <Typography
+                variant="h3"
+                gutterBottom
+                color="textPrimary"
+                component="h3"
+                align="center"
+                className="titulo"
+              >
+                Sobre nós!
+              </Typography>
               <Typography className="about">
                 Somos um grupo de Saúde e Bem-Estar, com foco em reforçar a
                 prevenção e o tratamento do abuso de drogas entorpecentes e uso
@@ -41,19 +32,74 @@ function Sobre() {
                 pelo usuário, dificuldade financeira tanto do usuário quanto de
                 familiares próximos, mortes evitáveis de recém-nascidos vindo de
                 mães alcoólatras ou usuárias de drogas (por conta da mãe a
-                criança pode nascer com alguma sequela).!
+                criança pode nascer com alguma sequela).
               </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <img
+              src="/src/assets/images/logo.png"
+              alt="Logo"
+              height="500px"
+              className="logo-image"
+            />
+          </Grid>
+        </Grid>
+      </div>
+      <Typography className="developers-title">
+        Conheça nossa equipe:
+      </Typography>
+      <div className="developers-section">
 
-          </Box>
+
+        <Grid container spacing={2} className="developers-container">
+          <Grid item xs={12} sm={6} md={4}>
+            <div className="developer">
+              <img className="fotos" src="/src/assets/images/FotosDevs/lucas.jpg" alt="Developer 1" />
+            </div>
+            <Typography className="dev-text" variant="subtitle1">Lucas Pinheiro</Typography>
+            <Typography variant="body2">Cargo/Função</Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <div className="developer">
+              <img className="fotos" src="/src/assets/images/FotosDevs/exemplo.png" alt="Developer 2" />         
+            </div>
+            <Typography className="dev-text" variant="subtitle1">Deborah Pantaleão</Typography>
+            <Typography variant="body2">Cargo/Função</Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <div className="developer">
+              <img className="fotos" src="/src/assets/images/FotosDevs/samuel.jpg" alt="Developer 3" />
+              
+            </div>
+            <Typography className="dev-text" variant="subtitle1">Samuel Souza</Typography>
+            <Typography variant="body2">Cargo/Função</Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <div className="developer">
+              <img className="fotos" src="/src/assets/images/FotosDevs/exemplo.png" alt="Developer 4" />              
+            </div>
+            <Typography className="dev-text" variant="subtitle1">Rafael Marques</Typography>
+            <Typography variant="body2">Cargo/Função</Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <div className="developer">
+              <img className="fotos" src="/src/assets/images/FotosDevs/exemplo.png" alt="Developer 5" />    
+            </div>
+            <Typography className="dev-text" variant="subtitle1">Caique Ramos</Typography>
+            <Typography variant="body2">Cargo/Função</Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <div className="developer">
+              <img className="fotos" src="/src/assets/images/FotosDevs/cinthia.png" alt="Developer 6" />
+            </div>
+            <Typography className="dev-text" variant="subtitle1">Cinthia Caroline</Typography>
+            <Typography variant="body2">Cargo/Função</Typography>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-        <img
-            src="/src/assets/images/logo.png"
-            height="500px"
-          />
-        </Grid>
-        <Grid xs={12} className="postagens"></Grid>
-      </Grid>
+      </div>
+
+      <Grid xs={12} className="postagens"></Grid>
     </>
   );
 }
--- src/services/Service.tsx
+++ src/services/Service.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create ({
-    baseURL: 'https://projetointegrador-xwx9.onrender.com/'
+    baseURL: 'https://projetointegrador-xwx9.onrender.com'
 })
 
 export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
