9a1fd802d1109b25caa83c05e6ce4dc79a4ee633
DehPantaleao <deborahpantaleao@hotmail.com>
2023-07-04 11:19:31 -0300

    Alterações Finais

--- src/services/Service.tsx
+++ src/services/Service.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create ({
-    baseURL: 'https://projetointegrador-xwx9.onrender.com'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
